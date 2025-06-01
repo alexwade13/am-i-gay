25c3d55c2c6a9c7dbdd438232056e7702b2da22a
Alex Wade <alex.wade13@gmail.com>
2025-06-01 19:21:22 -0400

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-responsive class="d-flex align-center justify-center fill-height">
+      <v-img
+        :src="yesImage"
+        alt="YES"
+        max-width="90vw"
+        max-height="90vh"
+        contain
+      />
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import yesImage from '@/assets/images/yes-word-made-shiny-lbgt-260nw-1097705531.jpg';
+</script>
+
+<style scoped>
+.fill-height {
+  min-height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+
+.v-responsive {
+  width: 100%;
+  height: 100%;
+}
+</style>
